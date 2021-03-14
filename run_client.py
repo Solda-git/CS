7782c740711ebc93949e6d61d78995e6d060633a
solda <soldatovsl@mail.ru>
2021-03-14 05:40:51 -0700

    client module changed. Added: create_message, recv_message, run  functions and client mode launch

--- run_client.py
+++ run_client.py
@@ -6,19 +6,24 @@ import json
 
 
 @click.command()
-@click.option('--mode', default='rw')
+
+#     mode of client:
+#     d - duplex mode (send and receive) - by default
+#     s - send mode only
+#     r - receive mode only
+@click.option('--mode', default='d')
 @click.option('--addr', default=CHAT_SERVER_IP_ADDRESS,help='Chat server IP-address')
 @click.option('--port', default=DEFAULT_PORT, help='Chat server port')
 def run_client(addr, port):
     print(f"run_client in {mode} mode" )
-    my_client = SChatClient(addr, port)
+    my_client = SChatClient(mode, addr, port)
     print(f"Client is connected to the address/port: {addr}/{port}")
-    my_client.send_message(my_client.client_socket,my_client.make_online())
-    # my_client.run()
-    try:
-        print(my_client.parse_server_answer(my_client.get_message(my_client.client_socket)))
-    except (ValueError, json.JSONDecodeError):
-        print("Can't decode server message")
+    # my_client.send_message(my_client.client_socket,my_client.make_online())
+    my_client.run()
+    # try:
+    #     print(my_client.parse_server_answer(my_client.get_message(my_client.client_socket)))
+    # except (ValueError, json.JSONDecodeError):
+    #     print("Can't decode server message")
 
 
 
--- run_server.py
+++ run_server.py
@@ -3,10 +3,11 @@ from chat.schatserver import SChatServer
 from lib.settings import DEFAULT_IP_ADDRESS, DEFAULT_PORT
 
 @click.command()
+@click.option('--mode', default='broadcast')
 @click.option('--addr', default=DEFAULT_IP_ADDRESS, help='IP address listening by server')
 @click.option('--port', default=DEFAULT_PORT, help='Listening port')
 def run_server(addr, port):
-    my_server = SChatServer(addr, port)
+    my_server = SChatServer(mode, addr, port)
     print(f'SChatServer run. Listening address: {("all addresses (by default)", addr)[bool(addr)]}, port: {port}')
     my_server.run()
 
